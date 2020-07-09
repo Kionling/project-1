1690d8d2c4a2d5e0fe0b7533086b0c8ae572f22c
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-07-08 19:09:58 -0700

    updated portfolio page

--- activities.js
+++ activities.js
@@ -10,50 +10,6 @@ function formatDuration(duration){
 
 }
 
-function formatTime(time) {
-    var timeMap = {
-        0: "12AM",
-        1: "1AM",
-        2: "2AM",
-        3: "3AM",
-        4: "4AM",
-        5: "5AM",
-        6: "6AM",
-        7: "7AM",
-        8: "8AM",
-        9: "9AM",
-        10: "10AM",
-        11: "11AM",
-        12: "12PM",
-        13: "1PM",
-        14: "2PM",
-        15: "3PM",
-        16: "4PM",
-        17: "5PM",
-        18: "6PM",
-        19: "7PM",
-        20: "8PM",
-        21: "9PM",
-        22: "10PM",
-        23: "11PM"
-    };
-    return timeMap[time];
-}
-function formatPlayBtn(cell){
-    return `
-        <iframe
-            scrolling="no"
-            frameborder="0"
-            allowTransparency="true"
-            src="https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=80&height=80&color=007FEB&layout=dark&size=small&type=tracks&id=${cell.getValue()}&app_id=1"
-            width="80"
-        height="80">
-        </iframe>
-    `;
-
-}
-
-
 var allActivities=JSON.parse(localStorage.getItem("activityPlaylist"));
 if(allActivities === null) {
     allActivities =[];
@@ -64,31 +20,29 @@ allActivities.forEach(function(activity, i){
     tabDiv.attr("id", "playlist-table-" + i);
     $("#tables").append(tabDiv);
 
-
     //parsing over playlist arr to get 
     var tabledata = activity.playlist.map(function(item) {
       var parsedItem = JSON.parse(item);
       parsedItem.duration = formatDuration(parsedItem.duration);
       return parsedItem;
-      
+
     });
     //create Tabulator on DOM element with id "example-table"
     var table = new Tabulator("#playlist-table-" + i, {
-        height:300, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+        height:205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
         data:tabledata, //assign data to table
         layout:"fitColumns", //fit columns to width of table (optional)
         columns:[ {//create column group
-            title: activity.name + " " +  formatTime(activity.time),
+            title: activity.name + " " + activity.time,
             columns:[
                 {title:"Title", field:"title", hozAlign:"center"},
                 {title:"Artist", field:"artist", hozAlign:"center"},
                 {title:"Album", field:"album", hozAlign:"center"},
-                {title:"Duration", field:"duration", hozAlign:"center"},
-                {title: "Play", field:"songId",hozAlign:"center",formatter:formatPlayBtn}
-
+                {title:"Duration", field:"duration", hozAlign:"center"}
             ],
         }],
       
     });
 
 });
+ 
